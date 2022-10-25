48d7ae56aaef6b15f4901470927e5775c79cf72b
Roger Krolow <roger.krolow@gmail.com>
2022-10-25 18:50:42 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from './components/Menu';
+
+function App() {
+  return (
+    <Menu/>
+  );
+}
+
+export default App;
---
+++ src/components/Menu/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <>
+            <menu>
+                <li>Custo de React JS</li>
+                <li><a href="#">Departamentos</a></li>
+                <li><a href="#">Add Departamento</a></li>
+            </menu>
+        </>
+    )
+}
+
+export default Menu;
